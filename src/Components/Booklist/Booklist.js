ffabed946a77f2fe20bfeb5a1cd3a49b8fed716d
Chandiran <chandiranvc2001@gmail.com>
2023-11-29 12:03:23 +0530

    edited shimmer

--- src/Components/Booklist/Booklist.js
+++ src/Components/Booklist/Booklist.js
@@ -1,23 +1,70 @@
-import React, { useContext } from 'react';
-import searchContext from '../../context/SearchContext';
-import coverNotFound from '../../Images/cover_not_found.jpg'
+import React, { useContext } from "react";
+import searchContext from "../../context/SearchContext";
+import coverNotFound from "../../Images/cover_not_found.jpg";
 
 const Booklist = () => {
-  const { data } = useContext(searchContext);
+  const { data, isLoading, resultTitle } = useContext(searchContext);
   const books = data?.slice(0, 24);
+  console.log(data);
+
+  const renderSkeleton = () => {
+    return (
+      <div className="flex flex-wrap justify-center">
+        {[...Array(12)].map((_, index) => (
+          <div
+            key={index}
+            className="bg-slate-100 w-72 h-80 p-2 m-5 mt-20 shadow-lg rounded-lg flex flex-col items-center animate-pulse"
+          >
+            <div className="h-52 w-32 m-3 shadow-lg bg-slate-300"> </div>
+            <div className="text-center">
+              <p className="h-5 w-48 m-2 rounded-full bg-slate-300"></p>
+              <p className="h-4 w-48 m-2 rounded-full bg-slate-200"></p>
+              <p className="h-4 w-48 m-2 rounded-full bg-slate-200"></p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <div className='flex flex-wrap justify-center'>
-      {books?.map((book, index) => (
-        <div key={index} className='bg-slate-100 w-72 p-2 m-5 mt-20 shadow-lg rounded-lg flex flex-col items-center transition-all hover:scale-105'>
-          <img className='h-52 m-3 shadow-lg' src={book.cover_i?`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`: coverNotFound} alt="" />
-          <div className='text-center'>
-            <p className='font-bold text-lg'>{book.title}</p>
-            <p className='font-semibold text-'>Author: {book.author_name}</p>
-            <p className='font-medium'>First Published Year: {book.first_publish_year}</p>
-          </div>
-        </div>
-      ))}
+    <div>
+      <div className="flex flex-wrap justify-center">
+        {isLoading ? (
+          renderSkeleton()
+        ) : (
+          <>
+            <p className="text-2xl font-medium mx-5 mt-5 w-full">
+              {resultTitle}
+            </p>
+            {books?.map((book, index) => (
+              <div
+                key={index}
+                className="bg-slate-100 w-72 p-2 m-5 mt-20 shadow-lg rounded-lg flex flex-col items-center transition-all hover:scale-105"
+              >
+                <img
+                  className="h-52 m-3 shadow-lg"
+                  src={
+                    book.cover_i
+                      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+                      : coverNotFound
+                  }
+                  alt="book cover"
+                />
+                <div className="text-center">
+                  <p className="font-bold text-lg">{book.title}</p>
+                  <p className="font-semibold text-">
+                    Author: {book.author_name}
+                  </p>
+                  <p className="font-medium">
+                    First Published Year: {book.first_publish_year}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/context/SearchContextProvider.js
+++ src/context/SearchContextProvider.js
@@ -3,23 +3,35 @@ import SearchContext from "./SearchContext";
 
 const SearchContextProvider = ({ children }) => {
   const [searchValue, setSearchValue] = useState("the lost world");
+  const [resultTitle, setresultTitle] = useState("");
   const [data, setData] = useState(null);
   const URL = "http://openlibrary.org/search.json?title=";
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(`${URL}${searchValue}`);
-      const data = await response.json();
-      setData(data?.docs);
-      setIsLoading(false);
-    }
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${URL}${searchValue}`);
+        const result = await response.json();
+        setData(result?.docs);
 
+        setresultTitle(
+          result?.docs.length > 1 ? "Your search Result" : "No result found"
+        );
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    }
     fetchData();
   }, [searchValue]);
 
   return (
-    <SearchContext.Provider value={{ searchValue, setSearchValue, data }}>
+    <SearchContext.Provider value={{ searchValue, setSearchValue, data, isLoading, resultTitle}}>
       {children}
     </SearchContext.Provider>
   );
