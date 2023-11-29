ddf10e6c3ef9c49528f1b4701aff6139892d9703
Chandiran <chandiranvc2001@gmail.com>
2023-11-29 20:35:10 +0530

    added bg color

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import Bookdetials from './Components/Bookdetials/Bookdetials'
 const App = () => {
   return (
     <>
-    <Header/>
+    <div className='z-[-2] h-full w-full transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]'>
+      <Header/>
     <Body/>
+    </div>
+    
     </>
   )
 }
